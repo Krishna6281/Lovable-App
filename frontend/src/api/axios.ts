d57048e1feea52b2b5ef85f54d8068cc21e7adf3
krishna <ambavaramkr6964@gmail.com>
2026-03-05 19:00:40 +0530

    new

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bright-basket.onrender.com",
+  baseURL: "",
 });
 
 // 🔑 Attach JWT automatically (ADMIN calls)
--- frontend/src/api/productApi.ts
+++ frontend/src/api/productApi.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8081/api/products";
+const BASE_URL = "http://localhost:8081/a";
 
 export const getAllProductsApi = async () => {
   const res = await fetch(BASE_URL);
